10755f8d41d371a58f6535d60d4f1b9719d9983a
Khaled Akkari <khaled_akkari@tn.ibm.com>
2023-06-17 05:09:09 +0100

    first commit

--- src/main/java/tn/khaled/eureka_server/EurekaServerApplication.java
+++ src/main/java/tn/khaled/eureka_server/EurekaServerApplication.java
@@ -1,4 +1,4 @@
-package com.seddik.mcroservice_project.eureka_server;
+package tn.khaled.eureka_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/tn/khaled/eureka_server/EurekaServerApplicationTests.java
+++ src/test/java/tn/khaled/eureka_server/EurekaServerApplicationTests.java
@@ -1,4 +1,4 @@
-package com.seddik.mcroservice_project.eureka_server;
+package tn.khaled.eureka_server;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
